7d3d103b6e042548923d84d0eb6d50cfafc67940
191G0243@rcarbonifera.tecnm.mx <191G0243@rcarbonifera.tecnm.mx>
2023-03-10 02:12:00 -0600

    getgrupos

--- AppDocenteAsignatura/ViewModels/DocenteAsignaturaViewModel.cs
+++ AppDocenteAsignatura/ViewModels/DocenteAsignaturaViewModel.cs
@@ -1,7 +1,10 @@
-﻿using AppDocenteAsignatura.Views;
+﻿using AppDocenteAsignatura.Models;
+using AppDocenteAsignatura.Services;
+using AppDocenteAsignatura.Views;
 using Microsoft.Maui.Platform;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +12,31 @@ using System.Windows.Input;
 
 namespace AppDocenteAsignatura.ViewModels
 {
-    public class DocenteAsignaturaViewModel
+    public class DocenteAsignaturaViewModel:INotifyPropertyChanged
     {
 
         MainView mainView;
 
+        public List<Grupos> GruposDelDocente { get; set; }
         public ICommand LoginCommand { get; set; }
+
+        public DocenteAsignService service;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public DocenteAsignaturaViewModel()
         {
-            LoginCommand = new Command(IniciarSesion);
+            LoginCommand = new Command(IniciarSesionAsync);
+            service = new DocenteAsignService();
 
         }
 
-        private  void IniciarSesion()
+        private async void IniciarSesionAsync()
         {
-           
-            mainView = new MainView() { BindingContext = this };
+
+
+            GruposDelDocente =await service.GetGrupos(1);
+             mainView = new MainView() { BindingContext = this };
           
         Application.Current.MainPage = mainView;
 
